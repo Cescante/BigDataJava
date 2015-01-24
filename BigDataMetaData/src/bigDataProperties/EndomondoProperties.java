bfc9ae969c0df5f587caa68cbb6aecbe72cbb546
Cescante <aznranndydrakid@gmail.com>
2015-01-23 16:39:35 -0800

    First checkin of the BigDataJava project

---
+++ BigDataMetaData/src/bigDataProperties/EndomondoProperties.java
@@ -0,0 +1,22 @@
+package bigDataProperties;
+
+public final class EndomondoProperties
+{
+    /**
+     * Custom JSON property names.
+     */
+    public static final String workoutID = "sqlLable";
+    public static final String userID = "userID";
+    public static final String workoutData = "workoutData";
+    public static final String workoutSportName = "workoutSportName";
+    public static final String workoutDate = "workoutDate";
+    public static final String workoutDistance = "workoutDistance";
+    public static final String workoutDuration = "workoutDuration";
+    public static final String workoutAverageSpeed = "workoutAverageSpeed";
+    public static final String workoutMaxSpeed = "workoutMaxSpeed";
+    public static final String workoutCalories = "workoutCalories";
+    public static final String workoutMinAltitude  = "workoutMinAltitude";
+    public static final String workoutMaxAltitude = "workoutMaxAltitude";
+    public static final String workoutTotalAscent = "workoutTotalAscent";
+    public static final String workoutTotalDescent = "workoutTotalDescent";
+}
