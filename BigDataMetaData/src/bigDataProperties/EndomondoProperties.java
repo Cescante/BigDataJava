719f9cf83bc762ad3af06e7c3f68c0a00dc093a6
Cescante <aznranndydrakid@gmail.com>
2015-02-14 15:31:36 -0800

    New parsed properties.

--- BigDataMetaData/src/bigDataProperties/EndomondoProperties.java
+++ BigDataMetaData/src/bigDataProperties/EndomondoProperties.java
@@ -19,4 +19,31 @@ public final class EndomondoProperties
     public static final String workoutMaxAltitude = "workoutMaxAltitude";
     public static final String workoutTotalAscent = "workoutTotalAscent";
     public static final String workoutTotalDescent = "workoutTotalDescent";
+    
+    public static final String dataPoints = "dataPoints";
+    public static final String dataPointsLng = "dataPointsLng";
+    public static final String dataPointsLat = "dataPointsLat";
+    public static final String dataPointsDuration = "dataPointsDuration";
+    public static final String dataPointsDistance = "dataPointsDistance";
+    public static final String dataPointsPace = "dataPointsPace";
+    public static final String dataPointsAlt = "dataPointsAlt";
+
+    private static final String dataStartVertexLat = "dataStartVertexLat";
+    private static final String dataEndVertexLat = "dataEndVertexLat";
+    private static final String dataStartVertexLng = "dataStartVertexLng";
+    private static final String dataEndVertexLng = "dataEndVertexLng";
+    private static final String dataRecords = "dataRecords";
+    private static final String dataEncoded = "dataEncoded";
+
+    private static final String lapsAsc = "lapsAsc";
+    private static final String lapsDur = "lapsDur";
+    private static final String lapsMaxPace = "lapsMaxPace";
+    private static final String lapsDist = "lapsDist";
+    private static final String lapsAvgPace= "lapsAvgPace";
+    private static final String lapsEndLat = "lapsEnd_lat";
+    private static final String lapsEndLng = "lapsEnd_lng";
+    private static final String lapsMaxAlt = "lapsMaxAlt";
+    private static final String lapsMinAlt = "lapsMinAlt";
+    private static final String lapsDesc = "lapsDesc";
+    private static final String lapsPaths = "lapsPaths";
 }
